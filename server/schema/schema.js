609919393bb76dd99aeae1478db6949750894b1c
Emirhan Soylu <esoylu@corbone.com>
2022-02-05 21:48:52 +0300

    GraphQL Schema added

---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql')
+
+const { GraphQLObjectType } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: "Book",
+    fields: () => ({
+        id: { type: graphql.GraphQLString },
+        name: { type: graphql.GraphQLString },
+        genre: { type: graphql.GraphQLString },
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: graphql.GraphQLString } }
+            resolve(parent, args) {
+                //code to get data from db / other sources 
+            }
+        }
+    }
+});
+
+module.exports = new graphql.GraphQLSchema({
+    query: RootQuery,
+})
