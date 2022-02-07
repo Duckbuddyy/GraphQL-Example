89f3203a7747a63e8b2768f58cbb91a831418c54
Emirhan Soylu <emirhansoylu98@gmail.com>
2022-02-07 22:48:19 +0300

    Lecture 12

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,26 @@
 const graphql = require('graphql')
 const _ = require('lodash');
 
-const { 
+const {
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLString,
+    GraphQLInt,
     GraphQLID
 } = graphql;
 
 //dummy data
 var books = [
-    { name: "Name of the Wind", genre: "Fantasy", id: "1"},
-    { name: "The Final Empire", genre: "Fantasy", id: "2"},
-    { name: "The Long Earth", genre: "Sci-Fi", id: "3"},
-]
+    { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+    { name: "The Final Empire", genre: "Fantasy", id: "2" },
+    { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
+var authors = [
+    { name: "Patrick Rothfuss", age: 42, id: "1" },
+    { name: "Brandon Sanderson", age: 44, id: "2" },
+    { name: "Terry Pratchett", age: 66, id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
     name: "Book",
@@ -21,6 +28,15 @@ const BookType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
     })
 });
 
@@ -32,8 +48,15 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //code to get data from db / other sources
-                console.log(typeof(args.id))
-                return _.find(books, {id: args.id});
+                console.log(typeof (args.id))
+                return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id })
             }
         }
     }
