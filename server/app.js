cd3ae9c24f6f9bdf04b93c97b9e74943201291a7
Emirhan Soylu <emirhansoylu98@gmail.com>
2022-02-13 17:18:13 +0300

    MongoDB connection added, dummy data deleted.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-const { NODE_PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE_NAME } = require("./config/config");
+const { NODE_PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE_NAME } = require("../config/config");
 
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
-const schema = require("./server/schema/schema");
+const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 
 const mongoUrl = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@graphql-cluster.0suav.mongodb.net/${MONGO_DATABASE_NAME}?retryWrites=true&w=majority`;
