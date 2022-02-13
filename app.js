985654c86f7e34f77ea74601a0d8a7a788004f42
Emirhan Soylu <emirhansoylu98@gmail.com>
2022-02-13 16:50:07 +0300

    Filing updated, environment parameters added for development.

---
+++ app.js
@@ -0,0 +1,26 @@
+const { NODE_PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE_NAME } = require("./config/config");
+
+const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const schema = require("./server/schema/schema");
+const mongoose = require("mongoose");
+
+const mongoUrl = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@graphql-cluster.0suav.mongodb.net/${MONGO_DATABASE_NAME}?retryWrites=true&w=majority`;
+
+console.log(mongoUrl)
+
+const app = express();
+
+mongoose.connect(mongoUrl);
+mongoose.connection.once("open", () => {
+    console.log("Connected to database");
+});
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+
+app.listen(NODE_PORT, () => {
+    console.log("Listening request from localhost: " + NODE_PORT)
+});
---
+++ config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    MONGO_DATABASE_NAME: process.env.MONGO_DATABASE_NAME,
+    MONGO_USERNAME: process.env.MONGO_USERNAME,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    NODE_PORT: process.env.NODE_PORT,
+}
