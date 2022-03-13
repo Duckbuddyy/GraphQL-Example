588d41ea14739dd3b8a4f744e13d353fe02cc219
Emirhan Soylu <emirhansoylu98@gmail.com>
2022-03-13 18:22:13 +0300

    AddBook Component onChange methods added.

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,21 +3,26 @@ import { useQuery } from '@apollo/client';
 import { getAuthorsQuery } from '../queries/queries.js';
 
 const AddBook = (props) => {
+    let state = {
+        name: "1",
+        genre: "1",
+        authorId: "1"
+    };
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={(e) => submitForm(e, state)}>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text" />
+                <input type="text" onChange={(e) => state.name = e.target.value} />
             </div>
 
             <div className="field">
                 <label>Genre:</label>
-                <input type="text" />
+                <input type="text" onChange={(e) => state.genre = e.target.value} />
             </div>
 
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select onChange={(e) => state.authorId = e.target.value}>
                     <option>Select author</option>
                     {authorField()}
                 </select>
@@ -39,6 +44,11 @@ const authorField = (props) => {
         return data.authors.map(author =>
             <option key={author.id} value={author.id}>{author.name}</option>
         );
+}
+
+function submitForm(e, state) {
+    e.preventDefault();
+    console.log(state);
 }
 
 export default AddBook;
